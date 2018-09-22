7ec4acf40953e4ea921c0ebfd8399626b229c706
myasinK <yasinul@gmail.com>
2018-09-22 16:42:59 -0700

    Find if sum of powers of digits in a long int has factor

---
+++ Kata-PlayingWithDigits/DigPow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Kata_PlayingWithDigits
+{
+    public class DigPow
+    {
+        public static long digPow(int n, int p)
+        {
+            var m = CalcSumOfPowers(n, p);
+            return (m % n == 0) ? (long)m / n : -1;
+        }
+
+        public static int CalcSumOfPowers(int n, int p)
+        {
+            var nList = n.ToString().ToList();
+            int sum = 0;
+            foreach (var c in nList)
+            {
+                var cToNum = char.GetNumericValue(c);
+                sum += (int)Math.Pow(cToNum, p);
+                p++;
+            }
+
+            Console.WriteLine(sum);
+            return sum;
+        }
+    }
+}
